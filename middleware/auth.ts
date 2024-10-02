ddc1ed48666178a8a40f3795a4ebc3c10bc4851a
vazkya <142280637+vazkya@users.noreply.github.com>
2024-10-02 13:20:57 +0700

    Add files via upload

--- middleware/auth.ts
+++ middleware/auth.ts
@@ -1,6 +1,6 @@
 export default defineNuxtRouteMiddleware((to, _from) => {
-  const user = useSupabaseUser();
-  if (user.value?.user_metadata.tipe_user != "admin") {
-    return navigateTo("/");
-  }
+  // const user = useSupabaseUser();
+  // if (!user.value){
+  //   return navigateTo("/login");
+  // }
 });
