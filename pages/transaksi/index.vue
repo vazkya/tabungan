4dfe8dc496b726182f7860744256485ff8494543
vazkya <142280637+vazkya@users.noreply.github.com>
2024-10-31 11:51:44 +0700

    Update index.vue

--- pages/transaksi/index.vue
+++ pages/transaksi/index.vue
@@ -1,75 +1,76 @@
 <template>
     <div class="container-fluid">
-        <div class="row">
-            <h2 class="text-center my-4" style="color: black;">RIWAYAT TRANSAKSI</h2>
-            <div class="col-lg-12">
-            <nuxt-link to="/admin">
-                <button type="button" class="btn btn-lg rounded-5 px-5 bg-secondary text-white mb-3 float-lg-end">
-                KEMBALI
-                </button>
-            </nuxt-link>
-            <div class="table-responsive">
-                <table class="table table-striped">
-                <thead>
-                    <tr>
-                    <th>No</th>
-                    <th>NAMA</th>
-                    <th>TANGGAL</th>
-                    <th>BULAN</th>
-                    <th>KEPERLUAN</th>
-                    <th>JUMLAH</th>
-                    <th>AKSI</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr v-for="(visitor, i) in visitors" :key="visitor.id">
-                    <td>{{ i + 1 }}</td>
-                    <td>{{ visitor.nama.nama }}</td>
-                    <td>{{ visitor.tanggal }}</td>
-                    <td>{{ visitor.bulan.nama }}</td>
-                    <td>{{ visitor.keperluan.nama }}</td>
-                    <td>Rp. {{ visitor.jumlah }}</td>
-                    <td>
-                        <button @click="editData(visitor)" class="btn btn-primary btn-sm mb-2">Edit</button><br>
-                        <button @click="deleteTransaction(visitor.id)" class="btn btn-danger btn-sm">Hapus</button>
-                    </td>
-                    </tr>
-                </tbody>
-                </table>
-            </div>
-    
-            <!-- Modal for Editing Transaction -->
-            <div v-if="isEditing" class="modal">
-                <div class="modal-content">
-                <h3>EDIT TRANSAKSI</h3>
-                <div class="form-group">
-                    <label for="tanggal">Tanggal</label>
-                    <input id="tanggal" type="date" v-model="selectedTransaction.tanggal" />
-                </div>
-                <div class="form-group">
-                    <label for="bulan">Bulan</label>
-                    <input id="bulan" v-model="selectedTransaction.bulan.id" />
-                </div>
-                <div class="form-group">
-                    <label for="keperluan">Keperluan</label>
-                    <input id="keperluan" v-model="selectedTransaction.keperluan.id" />
-                </div>
-                <div class="form-group">
-                    <label for="jumlah">Jumlah</label>
-                    <input id="jumlah" type="number" v-model="selectedTransaction.jumlah" />
-                </div>
-                <div class="button-group">
-                    <button @click="updateData" class="btn simpan bg-primary">SIMPAN</button>
-                    <button @click="isEditing = false" class="btn batal bg-secondary">BATAL</button>
-                </div>
-                </div>
-            </div>
-            </div>
-        </div>
-    </div>
-</template>
+      <div class="row">
+        <h2 class="text-center my-4" style="color: black;">RIWAYAT TRANSAKSI</h2>
+        <div class="col-lg-12">
+          <nuxt-link to="/admin">
+            <button type="button" class="btn btn-lg rounded-5 px-5 bg-secondary text-white mb-3">
+              KEMBALI
+            </button>
+          </nuxt-link>
+          
+          <div class="table-responsive">
+            <table class="table table-striped">
+              <thead>
+                <tr>
+                  <th>No</th>
+                  <th>NAMA</th>
+                  <th>TANGGAL</th>
+                  <th>BULAN</th>
+                  <th>KEPERLUAN</th>
+                  <th>JUMLAH</th>
+                  <th>AKSI</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr v-for="(visitor, i) in visitors" :key="visitor.id">
+                  <td>{{ i + 1 }}</td>
+                  <td>{{ visitor.nama.nama }}</td>
+                  <td>{{ visitor.tanggal }}</td>
+                  <td>{{ visitor.bulan.nama }}</td>
+                  <td>{{ visitor.keperluan.nama }}</td>
+                  <td>Rp. {{ visitor.jumlah }}</td>
+                  <td>
+                    <button @click="editData(visitor)" class="btn btn-primary btn-sm mb-2">Edit</button><br>
+                    <button @click="deleteTransaction(visitor.id)" class="btn btn-danger btn-sm">Hapus</button>
+                  </td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
   
-<script setup>
+          <!-- Modal for Editing Transaction -->
+          <div v-if="isEditing" class="modal">
+            <div class="modal-content">
+              <h3>EDIT TRANSAKSI</h3>
+              <div class="form-group">
+                <label for="tanggal">Tanggal</label>
+                <input id="tanggal" type="date" v-model="selectedTransaction.tanggal" />
+              </div>
+              <div class="form-group">
+                <label for="bulan">Bulan</label>
+                <input id="bulan" v-model="selectedTransaction.bulan.id" />
+              </div>
+              <div class="form-group">
+                <label for="keperluan">Keperluan</label>
+                <input id="keperluan" v-model="selectedTransaction.keperluan.id" />
+              </div>
+              <div class="form-group">
+                <label for="jumlah">Jumlah</label>
+                <input id="jumlah" type="number" v-model="selectedTransaction.jumlah" />
+              </div>
+              <div class="button-group">
+                <button @click="updateData" class="btn simpan bg-primary">SIMPAN</button>
+                <button @click="isEditing = false" class="btn batal bg-secondary">BATAL</button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </template>
+  
+  <script setup>
   import { ref, onMounted } from 'vue';
   const supabase = useSupabaseClient();
   const visitors = ref([]);
